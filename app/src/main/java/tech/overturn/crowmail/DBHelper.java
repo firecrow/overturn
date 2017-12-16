a727614559315772eed887ad2ad1c03faf55faf5
firecrow silvernight <firecrow@new-host-3.home>
2017-12-15 21:23:31 -0500

    fleshing out db helper and adding an instantition in MainActivity

--- app/src/main/java/tech/overturn/crowmail/DBHelper.java
+++ app/src/main/java/tech/overturn/crowmail/DBHelper.java
@@ -4,19 +4,25 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import tech.overturn.crowmail.models.Account;
 
-/**
- * Created by firecrow on 12/15/17.
- */
-
 public class DBHelper {
     public static final int DB_VERSION = 1;
     public static final String DB_FNAME 'Crowmail.db';
 
-    Orm(Context ctx) {
+    DBHelper(Context ctx) {
         super(ctx, DB_FNAME, null, DB_VERSION);
     }
 
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(Orm.getCreateTable(Account));
+    public FeedReaderDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int old, int version) {
+        if(version == 1){
+            Log.d('----------- version is 1 --------:'+Orm.getCreateTable(Account));
+            //db.execSQL(Orm.getCreateTable(Account));
+        }
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
     }
 }
--- app/src/main/java/tech/overturn/crowmail/MainActivity.java
+++ app/src/main/java/tech/overturn/crowmail/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("fcrow","--------------------------------- hi -----------------------");
-        Log.d("fcrow", Orm.genCreateTable(Account.class));
-
+        DBHelper dbh = new DBHelper(getBaseContext());
     }
 }
