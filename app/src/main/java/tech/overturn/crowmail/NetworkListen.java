12dc3371439715f45d9c8a6a3c5282744ae7c1fb
Firecrow Silvernight <fire@firecrow.com>
2018-02-11 16:33:58 -0500

    making network callback more accurate

--- app/src/main/java/tech/overturn/crowmail/NetworkListen.java
+++ app/src/main/java/tech/overturn/crowmail/NetworkListen.java
@@ -6,11 +6,17 @@ import android.net.ConnectivityManager;
 import android.net.ConnectivityManager.NetworkCallback;
 import android.net.Network;
 import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
 import android.net.NetworkRequest;
 import android.util.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NetworkListen {
     public static boolean initialized = false;
+
+    public static Map<String, Boolean> interfacesUp = new HashMap<String, Boolean>();
 
     public static void listen(final Context context) {
         if(initialized){
@@ -18,44 +24,48 @@ public class NetworkListen {
         }
         initialized = true;
 
+        final ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
         NetworkCallback callback = new NetworkCallback() {
 
             @Override
             public void onAvailable(Network network) {
-                Log.d("fcrow", String.format("network up"));
-                Global.setNetworkUp(context, true);
+                NetworkInfo info = cm.getNetworkInfo(network);
+                if (info != null) {
+                    String name = info.getTypeName();
+                    interfacesUp.put(name, true);
+                    Log.d("fcrow", String.format("network up %s", name));
+                    NetworkListen.calcUp(context);
+                } else {
+                    Log.d("fcrow", String.format("network up null"));
+                }
             }
 
             @Override
             public void onUnavailable() {
                 Log.d("fcrow", String.format("network unavailable"));
-                Global.setNetworkUp(context, false);
+                NetworkListen.calcUp(context);
             }
 
             @Override
             public void onLost(Network network) {
-                Log.d("fcrow", String.format("network lost"));
-                Global.setNetworkUp(context, false);
-            }
-
-            @Override
-            public void onCapabilitiesChanged(Network network, NetworkCapabilities cap) {
-                Log.d("fcrow",
-                        String.format("network capabilities changed mobile:%b wifi:%b",
-                                cap.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR),
-                                cap.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
-                        )
-                );
+                NetworkInfo info = cm.getNetworkInfo(network);
+                if (info != null) {
+                    String name = info.getTypeName();
+                    interfacesUp.remove(name);
+                    Log.d("fcrow", String.format("network lost %s", name));
+                    NetworkListen.calcUp(context);
+                } else {
+                    Log.d("fcrow", String.format("network lost null"));
+                }
             }
         };
 
-        NetworkRequest req = new NetworkRequest.Builder()
-                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-                .build()
-                ;
+        cm.registerNetworkCallback(new NetworkRequest.Builder().build(), callback);
+    }
 
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        cm.registerNetworkCallback(req, callback);
+    public static void calcUp(Context context) {
+        Boolean up = interfacesUp.size() > 0;
+        Global.setNetworkUp(context, up);
     }
 }
